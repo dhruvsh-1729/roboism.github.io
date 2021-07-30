32ce3d6d61cb26a67e24ba735d9a12f6d773a689
sudoshreyansh <shreyanshjain14072002@gmail.com>
2021-07-30 19:48:16 +0530

    added about us demo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-document.querySelector('.menu__hamburger').addEventListener('click', () => {
-    document.querySelector('.mobile-menu').classList.toggle('active');
+let menuHamburger = document.querySelector('.menu__hamburger');
+let mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
 })
